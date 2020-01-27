a01b2b21f11c6818b0365fda2e578b9289b7de21
Jonathan Boyle <jonathan.boyle@nag.co.uk>
2020-01-27 13:36:48 +0000

    add inefficiency metrics

--- pypop/metrics/hybrid.py
+++ pypop/metrics/hybrid.py
@@ -10,7 +10,164 @@ import pandas
 
 from .metricset import MetricSet, Metric
 
-__all__ = ['MPI_OpenMP_Metrics', 'MPI_OpenMP_Multiplicative_Metrics']
+__all__ = ['MPI_OpenMP_Metrics', 'MPI_OpenMP_Multiplicative_Metrics', 'MPI_OpenMP_Ineff_Metrics']
+
+
+class MPI_OpenMP_Ineff_Metrics(MetricSet):
+    """Proposed Hybrid MPI+OpenMP Inefficiency Metrics.
+    """
+
+    _metric_list = [
+        Metric("Global Inefficiency", 0),
+        Metric("Parallel Inefficiency", 1),
+        Metric("Process Level Inefficiency", 2),
+        Metric("MPI Load Balance Inefficiency", 3),
+        Metric("MPI Communication Inefficiency", 3),
+        Metric("MPI Transfer Inefficiency", 4),
+        Metric("MPI Serialisation Inefficiency", 4),
+        Metric("Thread Level Inefficiency", 2),
+        Metric("OpenMP Region Inefficiency", 3),
+        Metric("Serial Region Inefficiency", 3),
+        Metric("Computational Scaling", 1),
+        Metric("Instruction Scaling", 2),
+        Metric("IPC Scaling", 2, "IPC Scaling"),
+    ]
+
+    def _calculate_metrics(self, ref_key=None, sort_keys=True):
+        if not ref_key:
+            ref_key = min(self._stats_dict.keys())
+
+        metrics_by_key = {}
+
+        if sort_keys:
+            keys = sorted(self._stats_dict.keys())
+        else:
+            key = self._stats_dict.keys()
+
+        for key in keys:
+            metadata = self._stats_dict[key].metadata
+            stats = self._stats_dict[key].stats
+            try:
+                nthreads = metadata.application_layout.rank_threads[0][0]
+                metrics = {"Number of Processes": sum(metadata.procs_per_node)}
+
+                metrics["OpenMP Region Inefficiency"] = (
+                    (
+                        (
+                            stats["OpenMP Total Runtime"].loc[:, 1]
+                            - stats["OpenMP Useful Computation"].mean(level="rank")
+                        ).mean()
+                    )
+                    / stats["Total Runtime"].max()
+                )
+
+                metrics["Serial Region Inefficiency"] = (
+                    stats["Serial Useful Computation"].loc[:, 1].mean()
+                    / stats["Total Runtime"].max()
+                    * (1 - 1 / nthreads)
+                )
+
+                metrics["Thread Level Inefficiency"] = (
+                    (
+                        stats["OpenMP Total Runtime"].loc[:, 1].mean()
+                        - stats["OpenMP Useful Computation"].mean()
+                        + stats["Serial Useful Computation"].loc[:, 1].mean()
+                        * (1 - 1 / nthreads)
+                    )
+                    / stats["Total Runtime"].max()
+                )
+
+                metrics["MPI Communication Inefficiency"] = 1 - (
+                    stats["Total Non-MPI Runtime"].loc[:, 1].max()
+                    / stats["Total Runtime"].max()
+                )
+                
+                try:
+                    metrics["MPI Serialisation Inefficiency"] = (
+                        (
+                            stats["Ideal Runtime"].loc[:, 1].max()
+                            - stats["Total Non-MPI Runtime"].loc[:, 1].max()
+                        )
+                        / stats["Total Runtime"].max()
+                    )
+                except KeyError:
+                    metrics["MPI Serialisation Inefficiency"] = numpy.nan
+
+                try:
+                    metrics["MPI Transfer Inefficiency"] = 1- (
+                        stats["Ideal Runtime"].loc[:, 1].max()
+                        / stats["Total Runtime"].max()
+                    )
+                except KeyError:
+                    metrics["MPI Transfer Inefficiency"] = numpy.nan
+                    
+                try:
+                    if stats["Total Non-MPI Runtime"].loc[:, 1].max() > stats["Ideal Runtime"].loc[:, 1].max() :
+                        raise RuntimeError('Illegal Ideal Runtime value')
+                except RuntimeError:
+                    metrics["MPI Serialisation Inefficiency"] = numpy.nan
+                    metrics["MPI Transfer Inefficiency"] = numpy.nan
+
+                metrics["MPI Load Balance Inefficiency"] = (
+                    (
+                        stats["Total Non-MPI Runtime"].loc[:, 1].max()
+                        - stats["Total Non-MPI Runtime"].loc[:, 1].mean()
+                    )
+                    / stats["Total Runtime"].max()
+                )
+
+                metrics["Process Level Inefficiency"] = 1 - (
+                    stats["Total Non-MPI Runtime"].loc[:, 1].mean()
+                ) / stats["Total Runtime"].max()
+
+                metrics["Parallel Inefficiency"] = 1- (
+                    stats["Total Useful Computation"].mean()
+                    / stats["Total Runtime"].max()  # avg all threads to include Amdahl
+                )
+
+                metrics["IPC Scaling"] = (
+                    stats["IPC"].mean() / self._stats_dict[ref_key].stats["IPC"].mean()
+                )
+
+                metrics["Instruction Scaling"] = (
+                    self._stats_dict[ref_key].stats["Useful Instructions"].sum()
+                    / stats["Useful Instructions"].sum()
+                )
+
+                metrics["Frequency Scaling"] = (
+                    stats["Frequency"].mean()
+                    / self._stats_dict[ref_key].stats["Frequency"].mean()
+                )
+
+                metrics["Computational Scaling"] = (
+                    self._stats_dict[ref_key].stats["Total Useful Computation"].sum()
+                    / stats["Total Useful Computation"].sum()
+                )
+
+                metrics["Global Inefficiency"] = ( 1 - 
+                    (
+                    metrics["Computational Scaling"] * 
+                        (
+                            1 - metrics["Parallel Inefficiency"]
+                        )
+                    )
+                )
+
+                metrics["Speedup"] = (
+                    self._stats_dict[ref_key].stats["Total Runtime"].max()
+                    / stats["Total Runtime"].max()
+                )
+
+                metrics["Runtime"] = stats["Total Runtime"].max()
+
+            except KeyError as err:
+                raise ValueError(
+                    "No '{}' statistic. (Wrong analysis type?)" "".format(err.args[0])
+                )
+
+            metrics_by_key[key] = metrics
+
+        self._metric_data = pandas.DataFrame(metrics_by_key).T
 
 
 class MPI_OpenMP_Metrics(MetricSet):
@@ -84,8 +241,12 @@ class MPI_OpenMP_Metrics(MetricSet):
 
                 try:
                     metrics["MPI Serialisation Efficiency"] = (
-                        stats["Total Non-MPI Runtime"].loc[:, 1].max()
-                        / stats["Ideal Runtime"].loc[:, 1].max()
+                        (
+                            stats["Total Runtime"].loc[:, 1].max()
+                            - stats["Total Non-MPI Runtime"].loc[:, 1].max()
+                            + stats["Ideal Runtime"].loc[:, 1].max()
+                        )
+                        / stats["Total Runtime"].max()
                     )
                 except KeyError:
                     metrics["MPI Serialisation Efficiency"] = numpy.nan
@@ -93,9 +254,16 @@ class MPI_OpenMP_Metrics(MetricSet):
                 try:
                     metrics["MPI Transfer Efficiency"] = (
                         stats["Ideal Runtime"].loc[:, 1].max()
-                        / stats["Total Runtime"].loc[:, 1].max()
+                        / stats["Total Runtime"].max()
                     )
                 except KeyError:
+                    metrics["MPI Transfer Efficiency"] = numpy.nan
+                    
+                try:
+                    if stats["Total Non-MPI Runtime"].loc[:, 1].max() > stats["Ideal Runtime"].loc[:, 1].max() :
+                        raise RuntimeError('Illegal Ideal Runtime value')
+                except RuntimeError:
+                    metrics["MPI Serialisation Efficiency"] = numpy.nan
                     metrics["MPI Transfer Efficiency"] = numpy.nan
 
                 metrics["MPI Load balance"] = 1 - (
