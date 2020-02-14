f7b9179f6cb33085a5450fd5ea6ed0b250736cf9
telemin <telemin@protonmail.ch>
2020-02-14 10:21:11 +0000

    begin validation development

--- pypop/notebook_interface.py
+++ pypop/notebook_interface.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env/python3
 
-from ipywidgets import VBox, GridBox, Button, Layout, Label, Tab
+from ipywidgets import (
+    Accordion,
+    Box,
+    HBox,
+    VBox,
+    Checkbox,
+    GridBox,
+    Button,
+    Layout,
+    Label,
+    Tab,
+)
 from ipyfilechooser import FileChooser
 
 from pypop.traceset import TraceSet
-from pypop.metrics import Judit_Hybrid_Metrics
 
 from .bokeh_widget import BokehWidget
 
@@ -20,13 +30,51 @@ from bokeh.models import HoverTool
 from bokeh.colors import RGB
 
 
+class ValidatingChooser(VBox):
+
+    _container_layout = Layout(margin="1em 0em 1em 0em")
+    _msgbox_layout = Layout()
+
+    def __init__(self, starting_file=None, **kwargs):
+
+        self._filechooser = FileChooser(filename=starting_file, select_default=True)
+        self._msgbox = HBox(children=[], layout=self._msgbox_layout)
+        self._validity = None
+
+        super().__init__(children=[self._filechooser], **kwargs)
+
+
+    def set_file_select_callback(self, callback):
+
+        self._filechooser.register_callback(callback)
+
+    @property
+    def validity(self):
+        return self._validity
+
+    @property
+    def files(self):
+        raise NotImplementedError
+
+    @property
+    def selected(self):
+        return self._filechooser.selected
+
+
 class FileSelector(VBox):
 
-    _add_row_button_layout = Layout(width="12em", margin="1em 0em 0em 0em")
+    _button_container_layout = Layout(
+        display="flex",
+        width="100%",
+        justify_content="space-between",
+        margin="1em 0em 1em 0em",
+        padding="0em 0em 0em 0em",
+    )
+    _control_button_layout = Layout(width="10em", margin="0em 2em 0em 0em")
     _label_layout = Layout(width="2em")
-    _remove_button_layout = Layout(width="3em")
-    _grid_layout = Layout(grid_template_columns="2em auto 3em", margin="1em 0em 0em 0em")
-    _calculate_button_layout = Layout(width="12em", margin="1em 0em 0em 0em")
+    _remove_button_layout = Layout(width="2.5em")
+    _grid_layout = Layout(grid_template_columns="2em auto 3em", margin="1em 0em 1em 0em")
+    _container_layout = Layout(padding="0em 1em 0em 1em", width="100%")
 
     def __init__(
         self, base_dir=".", starting_files=None, calculation_callback=None, **kwargs
@@ -35,39 +83,60 @@ class FileSelector(VBox):
         self._files = [None] if starting_files is None else starting_files
         self._calculation_callback = calculation_callback
 
-        self._add_row_button = None
         self._filechooser_grid = None
         self._filechoosers = None
-
         self._create_filechoosers()
-        self._create_buttons()
         self._update_filechooser_grid()
+
+        self._button_container = None
+        self._add_row_button = None
+        self._create_buttons()
+
+        self._advanced_config_controls = {}
+        self._advanced_config_box = None
+        self._advanced_config_accordion = None
+        self._create_advanced_config_box()
 
         super().__init__(
             children=[
-                self._add_row_button,
                 self._filechooser_grid,
-                self._calculate_button,
+                self._button_container,
+                self._advanced_config_accordion,
             ],
-            layout=Layout(width="auto"),
+            layout=self._container_layout,
             **kwargs
         )
 
     def _create_buttons(self):
         self._add_row_button = Button(
-            description="Add File",
-            button_style="info",
-            layout=self._add_row_button_layout,
+            description="Add File", layout=self._control_button_layout,
         )
         self._add_row_button.on_click(self._add_filechooser_row)
 
         self._calculate_button = Button(
-            description="Calculate Metrics",
+            description="Analyze",
             button_style="success",
-            layout=self._calculate_button_layout,
+            layout=self._control_button_layout,
         )
         if self._calculation_callback:
             self._calculate_button.on_click(self._calculation_callback)
+
+        self._button_container = Box(
+            children=[self._calculate_button, self._add_row_button],
+            layout=self._button_container_layout,
+        )
+
+    def _create_advanced_config_box(self):
+        self._advanced_config_controls["Delete Cache"] = Checkbox(
+            value=False, description="Delete Cache"
+        )
+
+        self._advanced_config_box = VBox(
+            children=tuple(self._advanced_config_controls.values())
+        )
+        self._advanced_config_accordion = Accordion(children=[self._advanced_config_box])
+        self._advanced_config_accordion.set_title(0, "Advanced Configuration")
+        self._advanced_config_accordion.selected_index = None
 
     def _add_filechooser_row(self, callback_reference=None):
 
@@ -77,10 +146,10 @@ class FileSelector(VBox):
     def _create_filechoosers(self):
 
         self._filechoosers = [
-            FileChooser(filename=fname, select_default=True) if fname else FileChooser()
+            ValidatingChooser(starting_file=fname) if fname else ValidatingChooser()
             for fname in self._files
         ]
-        [fc.register_callback(self._update_files) for fc in self._filechoosers]
+        [fc.set_file_select_callback(self._update_files) for fc in self._filechoosers]
 
     def _update_filechooser_grid(self):
         if self._filechooser_grid is None:
