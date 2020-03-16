f7c1075ff4fcaddc85224361195f8f66c104db46
telemin <telemin@protonmail.ch>
2020-03-16 11:40:06 +0000

    add pypop-copy-examples utility

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
             "pypop-openmp-metrics = pypop.cli:openmp_cli_metrics",
             "pypop-preprocess = pypop.cli:preprocess_traces",
             "pypop-idealise-prv = pypop.cli:dimemas_idealise",
+            "pypop-copy-examples = pypop.cli:copy_examples",
         ]
     },
 )
