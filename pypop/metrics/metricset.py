91e269357afcdd1222c46586fe9c4eeb447d43c5
telemin <telemin@protonmail.ch>
2020-03-19 16:36:09 +0000

    actually fix choosing of reference run

--- pypop/metrics/metricset.py
+++ pypop/metrics/metricset.py
@@ -109,7 +109,8 @@ class MetricSet:
         return min(
             stats_dict.items(),
             key=lambda x: "{:05}_{:05}".format(
-                x[1].metadata.total_threads, x[1].metadata.threads_per_process[0]
+                sum(x[1].metadata.threads_per_process),
+                x[1].metadata.threads_per_process[0],
             ),
         )[0]
 
